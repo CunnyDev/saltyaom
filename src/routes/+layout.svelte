7d38f259721a1c0ee3a9d52dc8b3561865f5799d
chayapatr <chayapatr.arc@gmail.com>
2022-10-26 23:35:45 +0700

    feat: add gallary

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,1 +1,7 @@
-<script>import "../app.postcss";</script><slot></slot>
+<script>
+	import '../app.postcss';
+</script>
+
+<div class="flex flex-col p-6 min-h-[100vh] justify-start items-center gap-6">
+	<slot />
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,30 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	const proofs: [
+		{
+			link: string;
+			img: string;
+		}
+	] = [
+		{
+			link: '',
+			img: ''
+		},
+		{
+			link: '',
+			img: ''
+		},
+		{
+			link: '',
+			img: ''
+		}
+	];
+</script>
+
+<h1 class="text-2xl">Proofs that <a href="https://github.com/saltyaom">Saltyaom</a> is 😭</h1>
+<div class="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-3 w-full">
+	{#each proofs as proof}
+		<div class="w-full h-full aspect-square bg-gray-400">
+			<img src={proof.img} alt="" />
+		</div>
+	{/each}
+</div>
