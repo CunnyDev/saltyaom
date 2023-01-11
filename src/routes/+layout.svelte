aa03a1e9d6eb6fe529751166c80df026ac6ed4a3
SaltyAom <35027979+SaltyAom@users.noreply.github.com>
2023-01-11 02:27:33 +0000

    😭😭😭
    😭😭😭
    
    feat: 177013
    feat: more cunny 😭😭😭 (#2)
    
    😭😭😭😭😭💢💢💢💢💢
    
    Co-authored-by: Nutthapat Pongtanyavichai <59821765+Leomotors@users.noreply.github.com>
    Co-authored-by: SaltyAom <35027979+SaltyAom@users.noreply.github.com>
    Co-authored-by: SaltyAom <saltyaom@gmail.com>

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <script>
-	import '../app.scss';
+  import '../app.scss';
 </script>
 
-<div class="flex flex-col p-6 min-h-[100vh] justify-start items-center gap-6">
-	<slot />
+<div class="flex flex-col p-6 min-h-[100vh] justify-start items-center gap-4">
+  <slot />
 </div>
